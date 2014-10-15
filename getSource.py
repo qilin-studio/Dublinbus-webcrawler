a05d39fb5de4c101bc8c5766da62eb099bd9e9ab
Lin Qi <kirin1989@gmail.com>
2014-10-15 22:43:03 +0100

    add webcrawler and sample data

--- getSource.py
+++ getSource.py
@@ -2,22 +2,17 @@
 
 import urllib2
 
-# get source code from a specific website
 url = 'http://www.dublinbus.ie/en/RTPI/Sources-of-Real-Time-Information/?searchtype=view&searchquery=262' # write URL here
       
 req = urllib2.Request(url)
 response = urllib2.urlopen(req)
 the_page = response.read() 
-# print the_page # test if fetch source code successfully
-# end - get source code
+print the_page
 
-
-# output source code to a temp file
-file = open("temp.txt", "w")
+file = open("output.txt", "w")
 print "Name of the file: ", file.name
 
 for line in the_page:
     file.write(line)
 
 file.close()
-# end - output temp file
